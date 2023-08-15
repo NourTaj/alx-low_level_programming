9cd6f89b384b7d6b618534fe30d8be548e6187d2
John King <email@example.com>
2023-08-15 15:00:39 -0700

    updated

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -17,7 +17,8 @@ int main(void)
 	unsigned long int bef1;
 	unsigned long int bef2;
 	unsigned long int aft1;
-	unsigned long int aft2;	
+	unsigned long int aft2;
+
 	printf("%lu", bef);
 	for (i = 1; i < 91; i++)
 	{
